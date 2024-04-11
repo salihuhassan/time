16f75af4cce31c96a7793cf4873b96dd1c8ef3d3
salihu <159777745+salihuhassan@users.noreply.github.com>
2024-04-11 09:31:29 +0100

    Add files via upload

---
+++ asset/js/naf.js
@@ -0,0 +1,15 @@
+// function grace(){
+//     document.write(" with varisocial services or public welfare. The ministrcollaborates")
+//     document.write("with varisocial services or public welfare. The ministry typically collaborateswith ous stakeholders, including healthcare professionals, public health")
+// }
+
+function verify(){
+    let use=  document.getElementById("user1").value;
+    let password =document.getElementById("pw2").value;
+    let cpassword = document.getElementById("cpw3" ).value;
+    
+    if(password=== cpassword){
+    document.write("password match" )
+    } else{ 
+    alert("not match")
+}}
